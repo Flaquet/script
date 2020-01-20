35f41a77e5498ba1902afd7362d0aa0b672cd70b
FLAQUET <55395047+Flaquet@users.noreply.github.com>
2020-01-20 16:51:27 +0100

    ouiiii

--- Exercice11/exercice11.js
+++ Exercice11/exercice11.js
@@ -1,14 +1,9 @@
-function modif_paragraphe(){
-
-    var tite = document.getElementById("h");
-
-    tite.style
-    
+function modif_paragraphe() {
+    var elem = document.getElementById('p');
+    elem.innerHTML = '<h1><I>Corrigé</I></h1>';
 }
-function centrage_h1(){
 
-    var para = document.getElementById("p");
-
-
-
+function centrage_h1() {
+    titre = document.getElementsByTagName("h");
+    titre.setAttribute("align", "center");
 }
--- Exercice12/exercice12.js
+++ Exercice12/exercice12.js
@@ -1,3 +1,9 @@
-funtion5050(){
-    window.resizeTo("10","10");
+function funtionouvrir(){
+    mywindow = window.open('', '', 'width=200, height=100');
+}
+function funtion5050(){
+    mywindow.moveTo("50","50");
+} 
+function function100(){
+    mywindow.resizeTo(100, 100)
 }
